2d35b7ddd48f83cb0e47769f38a5bdd551aaeb66
quic_swarbu <quic_swarbu@quicinc.com>
2024-11-06 14:57:26 +0530

    input: touchscreen: focaltech: fix pinctrl state in suspend resume
    In FT8726 touch controller, separate regulators are not needed. But
    regulator voting and pinctrl suspend / normal calls are done together.
    This leads to wrong pinctrl state during suspend leading to leakage.
    
    Change-Id: Ia9fe89db1c0c87fd02ee4a7cf6f5a485cb7e23d4
    Signed-off-by: quic_swarbu <quic_swarbu@quicinc.com>

--- focaltech_touch/focaltech_core.c
+++ focaltech_touch/focaltech_core.c
@@ -3062,6 +3062,11 @@ static int fts_ts_suspend(struct device *dev)
 				FTS_ERROR("power enter suspend fail");
 			}
 #endif
+		} else {
+#if FTS_PINCTRL_EN
+			fts_pinctrl_select_suspend(ts_data);
+#endif
+			gpio_direction_output(ts_data->pdata->reset_gpio, 0);
 		}
 #endif
 	}
@@ -3111,13 +3116,14 @@ static int fts_ts_resume(struct device *dev)
 #if FTS_POWER_SOURCE_CUST_EN
 		fts_power_source_resume(ts_data);
 #endif
-		fts_reset_proc(200);
+	} else {
+#if FTS_PINCTRL_EN
+		fts_pinctrl_select_normal(ts_data);
+#endif
 	}
-
-
+#endif
 	fts_reset_proc(200);
 
-#endif
 	fts_wait_tp_to_valid();
 	fts_ex_mode_recovery(ts_data);
 
